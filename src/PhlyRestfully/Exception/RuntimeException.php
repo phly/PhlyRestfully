ef41fc7d0f653788e5696c1264aadac82f77f8be
Matthew Weier O'Phinney <matthew@zend.com>
2013-01-09 14:42:45 -0600

    Implement item and collection serialization
    - Overrides serialize() method of JsonModel:
      - If an "item" key exists, attempts to serialize it with:
        - JsonSerializable
        - class/interface-specific hydrator
        - default hydrator (if any)
        - PHP typecast to array
      - If an iterable "items" key exists, iterates the collection, passing
        each member as above
    - allows specifying class/interface => hydrator pairs
    - allows specifying a default hydrator

---
+++ src/PhlyRestfully/Exception/RuntimeException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace PhlyRestfully\Exception;
+
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{
+}
--- src/PhlyRestfully/RestfulJsonModel.php
+++ src/PhlyRestfully/RestfulJsonModel.php
@@ -2,6 +2,11 @@
 
 namespace PhlyRestfully;
 
+use JsonSerializable;
+use Traversable;
+use Zend\Json\Json;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Stdlib\Hydrator\HydratorInterface;
 use Zend\View\Model\JsonModel;
 
 /**
@@ -9,6 +14,85 @@ use Zend\View\Model\JsonModel;
  */
 class RestfulJsonModel extends JsonModel
 {
+    /**
+     * Default hydrator to use if none provided for item class
+     * 
+     * @var HydratorInterface
+     */
+    protected $defaultHydrator;
+
+    /**
+     * Map of class/hydrator pairs
+     * 
+     * @var HydratorInterface[]
+     */
+    protected $hydrators = array();
+
+    /**
+     * Set the default hydrator to use as a fallback
+     * 
+     * @param  HydratorInterface $hydrator 
+     */
+    public function setDefaultHydrator(HydratorInterface $hydrator)
+    {
+        $this->defaultHydrator = $hydrator;
+    }
+
+    /**
+     * Add a class (or interface) => hydrator mapping
+     * 
+     * @param  string $class 
+     * @param  HydratorInterface $hydrator 
+     */
+    public function addHydrator($class, HydratorInterface $hydrator)
+    {
+        $this->hydrators[$class] = $hydrator;
+    }
+
+    /**
+     * Does a hydrator exist for the given class?
+     * 
+     * @param  object $item 
+     * @return bool
+     */
+    public function hasHydrator($item)
+    {
+        foreach (array_reverse($this->hydrators) as $test => $hydrator) {
+            if ($item instanceof $test) {
+                $class = get_class($item);
+                if ($class !== $test) {
+                    $this->addHydrator($class, $hydrator);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Retrieve a hydrator mapped to a given item
+     * 
+     * @param  object $item 
+     * @return HydratorInterface
+     * @throws Exception\RuntimeException if not found
+     */
+    public function getHydrator($item)
+    {
+        $class = get_class($item);
+        if (!$this->hasHydrator($item)) {
+            throw new Exception\RuntimeException(sprintf(
+                'Cannot retrieve hydrator for "%s"; no match found',
+                $class
+            ));
+        }
+        return $this->hydrators[$class];
+    }
+
+    /**
+     * Is this a problem api payload?
+     * 
+     * @return bool
+     */
     public function isProblemApi()
     {
         $variables = $this->getVariables();
@@ -21,6 +105,11 @@ class RestfulJsonModel extends JsonModel
         return false;
     }
 
+    /**
+     * Does this describe a HAL response?
+     * 
+     * @return bool
+     */
     public function isHal()
     {
         $variables = $this->getVariables();
@@ -29,5 +118,118 @@ class RestfulJsonModel extends JsonModel
             return true;
         }
         return false;
+    }
+
+    /**
+     * Serialize to JSON
+     *
+     * If the variables contain an object "item" key, the value is passed to
+     * serializeItem().
+     * 
+     * If the variables contain an iterable "items" key, the value is passed to
+     * serializeItems().
+     *
+     * Once all values have been filtered to serializeable form, the resulting 
+     * variables are serialized as JSON.
+     *
+     * @return string
+     */
+    public function serialize()
+    {
+        if ($this->isProblemApi()) {
+            return $this->serializeProblemApi();
+        }
+
+        $variables = $this->getVariables();
+        if ($variables instanceof Traversable) {
+            $variables = ArrayUtils::iteratorToArray($variables);
+        }
+
+        if (isset($variables['item']) && !is_object($variables['item'])) {
+            $variables['item'] = $this->serializeItem($variables['item']);
+        }
+
+        if (isset($variables['items'])
+            && (is_array($variables['items']) || $variables['items'] instanceof Traversable)
+        ) {
+            $variables['items'] = $this->serializeItems($variables['items']);
+        }
+
+        return Json::encode($variables);
+    }
+
+    /**
+     * Serialize an API-Problem payload
+     * 
+     * @return string
+     */
+    public function serializeProblemApi()
+    {
+        $variables = $this->getVariables();
+        if ($variables instanceof Traversable) {
+            $variables = ArrayUtils::iteratorToArray($variables);
+        }
+
+        $base = array(
+            'describedBy' => '',
+            'title'       => '',
+            'httpStatus'  => 500,
+            'detail'      => '',
+        );
+        $values = array_intersect_assoc($base, $variables);
+        return Json::encode($values);
+    }
+
+    /**
+     * Serialize a single item
+     *
+     * If the item implements JsonSerializable, it is returned as-is.
+     *
+     * If a hydrator has been mapped for the class of which $item is an 
+     * instance, that hydrator will be used to create and return an array 
+     * representation of the item.
+     *
+     * If a default hydrator exists, that hydrator will be used.
+     *
+     * Finally, if neither a class-specific or default hydrator is registered,
+     * the item will simply be cast to an array.
+     * 
+     * @param  object $item 
+     * @return array
+     */
+    protected function serializeItem($item)
+    {
+        if ($item instanceof JsonSerializable) {
+            return $item;
+        }
+
+        $hydrator = $this->getDefaultHydrator();
+        if (!$this->hasHydrator($item) && !$hydrator) {
+            return (array) $item;
+        }
+
+        if ($this->hasHydrator($item)) {
+            $hydrator = $this->getHydrator($item);
+        }
+
+        return $hydrator->extract($item);
+    }
+
+    /**
+     * Serialize a list of items
+     *
+     * Each item in $items is passed to serializeItem(), and appended
+     * to a new array.
+     * 
+     * @param  array|Traversable $items 
+     * @return array[]
+     */
+    protected function serializeItems($items)
+    {
+        $array = array();
+        foreach ($items as $item) {
+            $array[] = $this->serializeItem($item);
+        }
+        return $array;
     }
 }
