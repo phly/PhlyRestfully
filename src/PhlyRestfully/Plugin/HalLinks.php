4a3322c2c62504d2d861098c7e7da3f5e75b400d
Matthew Weier O'Phinney <matthew@zend.com>
2013-08-14 16:23:19 -0500

    [#95] Fixed issue
    - New link route option, "reuse_matched_params", can be used to set flag
      passed to url() helper. By default, if not passed, it will be assumed
      true.

--- src/PhlyRestfully/Plugin/HalLinks.php
+++ src/PhlyRestfully/Plugin/HalLinks.php
@@ -442,12 +442,19 @@ class HalLinks extends AbstractHelper implements
             );
         }
 
+        $reuseMatchedParams = true;
+        $options = $linkDefinition->getRouteOptions();
+        if (isset($options['reuse_matched_params'])) {
+            $reuseMatchedParams = (bool) $options['reuse_matched_params'];
+            unset($options['reuse_matched_params']);
+        }
+
         $path = call_user_func(
             $this->urlHelper,
             $linkDefinition->getRoute(),
             $linkDefinition->getRouteParams(),
-            $linkDefinition->getRouteOptions(),
-            true
+            $options,
+            $reuseMatchedParams
         );
 
         if (substr($path, 0, 4) == 'http') {
