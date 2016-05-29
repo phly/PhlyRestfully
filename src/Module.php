44bf16be0571d6ec225b02a19600738d0f2bcd1b
Andrew Caya <andrewscaya@yahoo.ca>
2016-05-29 19:07:36 -0400

    Modified `Module.php` to fix broken paths due to changes from 84a9235db19.

--- src/Module.php
+++ src/Module.php
@@ -23,7 +23,7 @@ class Module
     public function getAutoloaderConfig()
     {
         return ['Zend\Loader\StandardAutoloader' => ['namespaces' => [
-            __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+            __NAMESPACE__ => __DIR__,
         ]]];
     }
 
@@ -34,7 +34,7 @@ class Module
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     /**
